ea379d1bb9dabe423d5ea938d92a06c826cf053c
Peter Donald <peter@realityforge.org>
2011-05-20 18:26:39 +1000

    Remove methods that did not override

--- Happiness/HappinessViewController.m
+++ Happiness/HappinessViewController.m
@@ -51,14 +51,6 @@
   [super dealloc];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    // Releases the view if it doesn't have a superview.
-    [super didReceiveMemoryWarning];
-    
-    // Release any cached data, images, etc that aren't in use.
-}
-
 #pragma mark - View lifecycle
 
 - (void)viewDidLoad
@@ -72,12 +64,6 @@
 {
   [self releaseOutlets];
   [super viewDidUnload];
-}
-
-- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
-{
-    // Return YES for supported orientations
-    return YES;
 }
 
 @end
