2bde5964ad66694ec53d278861ec949bab666ec3
Peter Donald <peter@realityforge.org>
2011-05-22 12:26:43 +1000

    Initial Commit

---
+++ Psychologist/PsychologistAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  PsychologistAppDelegate.h
+//  Psychologist
+//
+//  Created by Peter Donald on 22/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PsychologistAppDelegate : NSObject <UIApplicationDelegate> {
+
+}
+
+@property (nonatomic, retain) IBOutlet UIWindow *window;
+
+@end
