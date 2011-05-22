b5b53e1de39a078dfad34a2725fdcc5415d4a6b3
Peter Donald <peter@realityforge.org>
2011-05-22 12:33:26 +1000

    Import FaceView and the HappinessViewController for the Happiness project

---
+++ Psychologist/HappinessViewController.h
@@ -0,0 +1,29 @@
+//
+//  HappinessViewController.h
+//  Happiness
+//
+//  Created by Peter Donald on 20/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "FaceView.h"
+
+@interface HappinessViewController : UIViewController <FaceViewDelegate> {
+  
+  // 0 (sad) to 100 (happy)
+  int happiness;   
+  
+  UISlider *slider;
+  FaceView *faceView;
+  
+}
+
+@property (retain) IBOutlet UISlider *slider;
+@property (retain) IBOutlet FaceView *faceView;
+
+@property (nonatomic) int happiness;
+
+-(IBAction) happinessChanged:(UISlider *)ignored;
+
+@end
---
+++ Psychologist/HappinessViewController.m
@@ -0,0 +1,69 @@
+//
+//  HappinessViewController.m
+//  Happiness
+//
+//  Created by Peter Donald on 20/05/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "HappinessViewController.h"
+
+@implementation HappinessViewController
+
+@synthesize slider;
+@synthesize faceView;
+@synthesize happiness;
+
+-(void)updateUIForHappinessChange
+{
+  self.slider.value = happiness / 100.0;
+  [self.faceView setNeedsDisplay];
+}
+
+- (void)setHappiness:(int)value
+{
+  if( value < 0 ) value = 0;
+  if( value > 100 ) value = 100;
+  happiness = value;
+  [self updateUIForHappinessChange];
+}
+
+-(IBAction) happinessChanged:(UISlider *)ignored
+{
+  self.happiness = (int)([slider value] * 100);
+}
+
+- (float)smileForFaceView:(FaceView *)requestor
+{
+  return (self.happiness - 50) / 50.0;
+}
+
+// Must be called during deallocation or unloading to release outlets
+- (void)releaseOutlets
+{
+  self.faceView = nil;
+  self.slider = nil;
+}
+
+- (void)dealloc
+{
+  [self releaseOutlets];
+  [super dealloc];
+}
+
+#pragma mark - View lifecycle
+
+- (void)viewDidLoad
+{
+  [super viewDidLoad];
+  self.faceView.delegate = self;
+  self.happiness = 50;
+}
+
+- (void)viewDidUnload
+{
+  [self releaseOutlets];
+  [super viewDidUnload];
+}
+
+@end
